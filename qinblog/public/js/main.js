9373afdadb1e396853c299771664908fddee5b9a
Roger Qin <evertqin@gmail.com>
2015-05-28 20:49:06 -0400

    changed the topbar above the navbar, so when you make the browser smaller, the topbar automatically disappear.

--- qinblog/public/js/main.js
+++ qinblog/public/js/main.js
@@ -2,52 +2,38 @@
  * Created by Roger on 5/23/2015.
  */
 
-(function($) {
-
-    $(document).ready(function() {
-        $(window).scroll(function() {
+(function ($) {
+    $(document).ready(function () {
+        $(window).scroll(function () {
             var height = $(window).scrollTop();
-
-            if(height == 0) {
-                $(".navbar-default").removeClass("sticky-scrolling");
-                $(".social-network-above-navbar").removeClass("sticky-top");
-                $(".social-network-above-navbar").addClass("nonsticky-top");
-
-
-
-            } else {
-
-
-                $(".navbar-default").addClass("sticky-scrolling");
-                $(".social-network-above-navbar").addClass("sticky-top");
-                $(".social-network-above-navbar").removeClass("nonsticky-top");
-
+            if($(window).width() > 768) {
+                if (height == 0) {
+                    $(".navbar-default").removeClass("sticky-scrolling");
+                    $(".social-network-above-navbar").removeClass("sticky-top");
+                } else {
+                    $(".navbar-default").addClass("sticky-scrolling");
+                    $(".social-network-above-navbar").addClass("sticky-top");
+                }
             }
 
-            if(height > 100) {
+            if (height > 100) {
                 $('.scroll-up').fadeIn();
             } else {
                 $('.scroll-up').fadeOut();
             }
-
         });
 
-        $('a[href="#totop"]').click(function() {
-            $('html, body').animate({ scrollTop: 0 }, 'slow');
+        $('a[href="#totop"]').click(function () {
+            $('html, body').animate({scrollTop: 0}, 'slow');
             return false;
         });
 
         var s = skrollr.init({
-            render:function(data) {
+            render: function (data) {
                 console.log(data.curTop);
             }
         });
-
-
-
-
     });
-
 
 
 })(jQuery);
