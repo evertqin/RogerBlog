42816f17c8bf9ec7addd5026de25b33f6434bb88
Roger Qin <evertqin@gmail.com>
2015-05-30 16:14:22 -0400

    Did refactoring, extra css for header navbar section, extract the jade for header navbar section. because index and blog pages share the same file snippet.

--- qinblog/public/js/blog/blog.main.js
+++ qinblog/public/js/blog/blog.main.js
@@ -8,6 +8,8 @@
             // The following code snippet demonstrate how to send a get request to the server
             // TODO: will comment out the following in the future
             var content = {test: $('input[name="fname"]').val()};
+            console.log(content);
+
             $.get('/blog/test', content, function(data) {
                 $('#result').html(data);
             });
@@ -16,6 +18,12 @@
                 console.log(data);
             });
 
+            $.get('/blog/userlist', content, function(data) {
+                for(var i = 0; i < data.userlist.length; ++i) {
+                    $('#result').html(data.userlist[i].email);
+                    console.log(data.userlist[i].email);
+                }
+            });
         });
 
     });
--- qinblog/routes/blog.js
+++ qinblog/routes/blog.js
@@ -3,6 +3,10 @@
  */
 var express = require('express');
 var router = express.Router();
+// support for mongodb
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/nodetest1');
 
 router.get('/',function(req, res, next) {
     console.log("Blogdddd");
@@ -11,11 +15,18 @@ router.get('/',function(req, res, next) {
 
 router.get('/test', function(req, res, next) {
    console.log(req.query.test);
-
 });
 
 router.get('/trial', function(req, res, next) {
    console.log(req.query.sample);
     res.send({iii:"dsds"});
 });
+
+router.get('/userlist', function(req, res, next) {
+    var collection = db.get('usercollection');
+    collection.find({},function(e, docs) {
+        res.send({userlist:docs});
+    });
+});
+
 module.exports = router;
