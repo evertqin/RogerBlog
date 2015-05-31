1963a552d2c2da6a9076728d851a8ed19955dc61
Roger Qin <evertqin@gmail.com>
2015-05-30 22:55:24 -0400

    add link from blog page to the landing page

--- qinblog/public/js/blog/blog.main.js
+++ qinblog/public/js/blog/blog.main.js
@@ -2,40 +2,47 @@
  * Created by Roger on 5/29/2015.
  */
 
-(function($) {
-    $(document).ready(function() {
-        $('input[name="submit"]').click(function() {
-            // The following code snippet demonstrate how to send a get request to the server
-            // TODO: will comment out the following in the future
-            var content = {test: $('input[name="fname"]').val()};
-            console.log(content);
+(function ($) {
+    $(document).ready(function () {
+        //$('input[name="submit"]').click(function() {
+        //    // The following code snippet demonstrate how to send a get request to the server
+        //    // TODO: will comment out the following in the future
+        //    var content = {test: $('input[name="fname"]').val()};
+        //    console.log(content);
+        //
+        //    $.get('/blog/test', content, function(data) {
+        //        $('#result').html(data);
+        //    });
+        //    // The following demonstrate how to send an ajax request and get back data from server
+        //    $.ajax('/blog/trial', {sample:"test"}).done(function(data) {
+        //        console.log(data);
+        //    });
+        //
+        //    $.get('/blog/userlist', content, function(data) {
+        //        for(var i = 0; i < data.userlist.length; ++i) {
+        //            $('#result').html(data.userlist[i].email);
+        //            console.log(data.userlist[i].email);
+        //        }
+        //    });
+        //});
 
-            $.get('/blog/test', content, function(data) {
-                $('#result').html(data);
+        (function () {
+            $('#navigation-items li>a').each(function() {
+                var orgHref = $(this).attr('href');
+                $(this).attr('href', '/' + orgHref);
             });
-            // The following demonstrate how to send an ajax request and get back data from server
-            $.ajax('/blog/trial', {sample:"test"}).done(function(data) {
-                console.log(data);
-            });
-
-            $.get('/blog/userlist', content, function(data) {
-                for(var i = 0; i < data.userlist.length; ++i) {
-                    $('#result').html(data.userlist[i].email);
-                    console.log(data.userlist[i].email);
-                }
-            });
-        });
+        }());
 
         // the code module is used to hide text when they get too long
-        (function() {
+        (function () {
             var showChar = 200; //
             var ellipsestext = '...';
             var moretext = 'more';
             var lesstext = 'less';
 
-            $('.more').each(function() {
+            $('.more').each(function () {
                 var content = $(this).html();
-                if(content.length > showChar) {
+                if (content.length > showChar) {
                     var c = content.substr(0, showChar);
                     var h = content.substr(showChar - 1, content.length - showChar);
                     var html = c + '<span class="moreellipses">' + ellipsestext + ' </span>';
