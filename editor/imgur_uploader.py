22bffd250f1d95c99f7321109672739f7b7db53d
Roger Qin <evertqin@gmail.com>
2015-07-14 22:24:17 -0400

    change fonts

--- editor/imgur_uploader.py
+++ editor/imgur_uploader.py
@@ -23,23 +23,35 @@ class ImageUploader:
     urlHash = None
     
     def __init__(self, imageList):
-        pin = None
-
-        authorization_url = self.client.get_auth_url('pin')
-        print("Go to the following link to obtain a pin: {0}".format(authorization_url))
-        pin = input("Enter pin code: ")
-        credentials = self.client.authorize(pin, 'pin')
-        self.client.set_user_auth(credentials['access_token'], credentials['refresh_token'])
-        logger.info("Authentication successful! Here are the details:")
-        logger.info("   Access token:  {0}".format(credentials['access_token']))
-        logger.info("   Refresh token: {0}".format(credentials['refresh_token']))
-
+        '''
+        First Check if we need to connect to imgur
+        '''
         if not os.path.isfile(self.GLOBAL_HASH):
             raise Exception("Cannot find the local path to url hash file {0}".format(self.GLOBAL_HASH))
-
         self.urlHash = json.load(open(self.GLOBAL_HASH))
-        
         self.imageList = imageList
+        isNeedImgurConnection = False
+        for image in imageList:
+            if image not in self.urlHash:
+                isNeedImgurConnection = True
+                break
+
+            
+        if isNeedImgurConnection:
+            pin = None
+            
+            authorization_url = self.client.get_auth_url('pin')
+            print("Go to the following link to obtain a pin: {0}".format(authorization_url))
+            pin = input("Enter pin code: ")
+            credentials = self.client.authorize(pin, 'pin')
+            self.client.set_user_auth(credentials['access_token'], credentials['refresh_token'])
+            logger.info("Authentication successful! Here are the details:")
+            logger.info("   Access token:  {0}".format(credentials['access_token']))
+            logger.info("   Refresh token: {0}".format(credentials['refresh_token']))
+
+
+
+
 
 
     def checkAuthenticationStatus(self):
@@ -78,8 +90,6 @@ class ImageUploader:
         config = {
             'album': self.getAlbumId("blog images"),
             'type': "file",
-            
-            
         }
         for path in self.imageList:
             if not os.path.isfile(path):
