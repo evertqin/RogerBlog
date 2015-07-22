12580b57788f79726fb8d54304ba85dd7bea95e1
Roger Qin <evertqin@gmail.com>
2015-07-21 21:01:13 -0400

    refine the digest

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ mongoClient.connect(mongoUrl, function(err, db) {
       for(var i = 0; i < data.length; ++i) {
         data[i].imgUrls = utils.extract_image_href(data[i].content);
         // reduce the data to send to front end
-        data[i].content = utils.get_first_several_p_tags(data[i].content);
+        data[i].content = utils.get_first_several_p_tags(data[i].content).substr(0, 200);
       //  data[i].content = utils.remove_image_href(data[i].content).substr(0, 200);
     }
       res.render('index', {posts:data, baseUrl:baseUrl});
