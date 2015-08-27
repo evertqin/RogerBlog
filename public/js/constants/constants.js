fc46cf7155ac05772278ebcfb2ea95204c7e5a3c
Roger Qin <evertqin@gmail.com>
2015-08-27 07:35:59 -0400

    add new images

---
+++ public/js/constants/constants.js
@@ -0,0 +1,12 @@
+
+  var constants = constants || {};
+  constants.staticImageUrls = [
+    '/images/background.png',
+    '/images/background_1.jpg',
+    '/images/IMG_9851_small.jpg',
+    '/images/IMG_1822_small.jpg',
+    '/images/IMG_3867_small.jpg',
+    '/images/IMG_9852_small.jpg',
+    '/images/IMG_0724_small.jpg',
+    '/images/denali_small.jpg'
+  ];
