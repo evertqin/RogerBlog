9d2e0bff7dfb6a7d1a74cccf85423b55d2693857
Roger Qin <evertqin@gmail.com>
2015-09-11 08:28:15 -0400

    replace less with sass because people say it is better

--- public/js/constants/constants.js
+++ public/js/constants/constants.js
@@ -10,3 +10,14 @@
     '/images/IMG_0724_small.jpg',
     '/images/denali_small.jpg'
   ];
+
+  var quotes = [
+    {
+      content: 'Think before you speak. Read before you think.',
+      author:'Fran Lebowitz, The Fran Lebowitz Reader'
+    },
+    {
+      content:'It is not how much we have, but how much we enjoy, that makes happiness.',
+      author:'Charles Spurgeon'
+    }
+  ];
--- scripts/transcriptless.sh
+++ scripts/transcriptless.sh
@@ -5,10 +5,11 @@ stylesheet_folder="./public/stylesheets"
 for file in `ls ${stylesheet_folder}`;
 do
     extension="${file##*.}"
-    if [ "$extension" = "less" ]; then
+    if [ "$extension" = "scss" ]; then
         filename="${file%.*}"
         echo "processing "$filename
-        lessc $stylesheet_folder/$file > $stylesheet_folder/${filename}.css
+        #lessc $stylesheet_folder/$file > $stylesheet_folder/${filename}.css
+        sass $stylesheet_folder/$file:$stylesheet_folder/${filename}.css
     fi
 done
 
