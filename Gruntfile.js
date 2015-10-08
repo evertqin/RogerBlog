c799a11f5185a02f4d241e9edde1e429afc80103
Roger Qin <evertqin@gmail.com>
2015-10-07 21:34:47 -0400

    commit incase of messing up

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,32 @@
 module.exports = function(grunt) {
     grunt.initConfig({
+        clean: ['build'],
+
         pkg: grunt.file.readJSON('package.json'),
+
+        sass: {
+          dynamic_mapping: {
+              files: [{
+                expand:true,
+                cwd:'public/stylesheets',
+                src:['*.scss'],
+                dest:'public/build/stylesheets',
+                ext:'.css',
+              }]
+            }
+        },
+        cssmin:{
+          dynamic_mapping: {
+              files: [{
+                expand:true,
+                cwd:'public/build/stylesheets',
+                src:['*.css'],
+                dest:'public/build/stylesheets',
+                ext:'.css',
+              }]
+            }
+        },
+
         uglify: {
             dynamic_mapping: {
                 files: [
@@ -8,22 +34,30 @@ module.exports = function(grunt) {
                     expand: true,
                     cwd:'public/js',
                     src:'**/*.js',
-                    dest:'public/build/',
+                    dest:'public/build/js',
                     ext:'.js',
-                        extDot: 'first'
+                    extDot: 'last'
                     },
                 ],
             },
         },
-        clean: {
 
+        copy:{
+          main: {
+
+          },
         }
+
     });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['uglify','sass','cssmin']);
 
 };
