617562dde431b4e72a45aca324026cc2310745e1
Ruogu Qin <ruoguqin@Ruogus-MacBook-Air.local>
2015-10-10 21:05:44 -0400

    update package

--- routes/photography.js
+++ routes/photography.js
@@ -7,19 +7,11 @@ var utils = require('../models/business_logic/utils/utils.js');
 var logger = require('../models/logging/logger.js');
 var mongoClient = require('mongodb').MongoClient;
 var redisClient = require('../models/redis/redis.js');
-var cloudinary = require('cloudinary');
 var Q = require('q');
 
-cloudinary.config({
-  cloud_name: 'evertqin',
-  api_key: '134222639559228',
-  api_secret: 'Uh9t8SpeCbTtBwBEVUssZiZQWGc'
-});
-
 router.get('/', function(req, res, next){
-  var imageUrl = cloudinary.url("sample.jpg", { width: 100, height: 150, crop: 'fill' } );
-  logger.info("this is called");
-  res.render('photography',{images:[imageUrl]});
+
+  res.render('photography');
 });
 
 module.exports = router;
