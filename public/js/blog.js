1b7c88f4841736557e3fe40e0e0dc9312d1c8f9a
Roger Qin <evertqin@gmail.com>
2015-12-03 08:14:06 -0500

    update blog

--- public/js/blog.js
+++ public/js/blog.js
@@ -2,7 +2,7 @@
 requirejs(['./common'], function (common) {
     requirejs(['/build/js/app/blog/blog.main.js']);
     requirejs(['/build/js/app/blog/blog_entries.js']);
-    requirejs(['/build/js/app/blog/blog_pics.js']);
-    requirejs(['/build/js/app/blog/blog_tagList.js']);
+    //requirejs(['/build/js/app/blog/blog_pics.js']);
+    //requirejs(['/build/js/app/blog/blog_tagList.js']);
 
 });
